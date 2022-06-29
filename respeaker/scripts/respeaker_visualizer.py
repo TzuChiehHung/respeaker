687100dd7064166218cccad8cdcd54d9cc764979
tc.hung <hungtc@solab.me.ntu.edu.tw>
2022-06-29 15:56:12 +0800

    add power spectral density visualization

--- respeaker/scripts/respeaker_visualizer.py
+++ respeaker/scripts/respeaker_visualizer.py
@@ -7,33 +7,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+from scipy import signal
+
 
 class RespeakerVisualizer:
 
 
     def __init__(self):
-        self.fig, self.ax = plt.subplots()
-        self.ln, = plt.plot([], [], 'b-')
-        self.x_data, self.y_data = [] , []
+        self.fig, self.axs = plt.subplots(2,1)
+
+        self.fs = 16000
+        self.data = []
+        self.chunk = 1024
+        self.t = np.arange(0, self.chunk)
+
+        # time series
+        self.raw, = self.axs[0].plot([], [], 'b-')
+
+        # frequency domain
+        self.psd, = self.axs[1].plot([], [], 'b-')
 
     def plot_init(self):
-        self.ax.set_xlim(0, 1024)
-        self.ax.set_ylim(-32768, 32768)
-        return self.ln
+        self.axs[0].set_xlim(0, self.chunk)
+        self.axs[0].set_ylim(-32768, 32768)
+
+        self.axs[1].set_xlim(0, self.fs//2)
+        self.axs[1].set_ylim(-60, 60)
+        return
 
     def audio_callback(self, msg):
-        self.x_data = np.arange(0, len(msg.data))
-        self.y_data = msg.data
+        if self.fs != msg.frame_rate or self.chunk != len(msg.data):
+            self.fs = msg.frame_rate
+            self.chunk = len(msg.data)
+            self.t = np.arange(0, self.chunk)
+            self.plot_init()
+
+        self.data = msg.data
 
     def update_plot(self, frame):
-        self.ax.set_xlim(0, len(self.x_data))
-        self.ln.set_data(self.x_data, self.y_data)
-        return self.ln
+        self.raw.set_data(self.t, self.data)
+
+        f, dbhz = self.power_spectral_density(self.data, self.fs)
+        self.psd.set_data(f, dbhz)
+        return
+
+    def power_spectral_density(self, data, sample_rate):
+        f, Pxxf = signal.welch(
+            data,
+            sample_rate,
+            nperseg=1024,
+            return_onesided=True,
+            detrend='constant',
+            scaling='density')
+        dbhz = 10*np.log10(Pxxf)
+        return f, dbhz
 
 
 if __name__ == '__main__':
     rospy.init_node('respeaker_visualizer', anonymous=True)
     vis = RespeakerVisualizer()
-    sub = rospy.Subscriber('audio', RespeakerMsg, vis.audio_callback)
-    ani = FuncAnimation(vis.fig, vis.update_plot, init_func=vis.plot_init)
+    sub = rospy.Subscriber('/respeaker/audio', RespeakerMsg, vis.audio_callback)
+    ani = FuncAnimation(vis.fig, vis.update_plot, init_func=vis.plot_init, interval=100)
     plt.show(block=True)
